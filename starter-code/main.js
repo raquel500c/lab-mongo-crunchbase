9b74b34c53d39e22eea6fdbf74817adabbc4bb2b
raquel500c <raquel500c@gmail.com>
2017-09-10 03:00:43 +0200

    query 5 -Done

--- starter-code/main.js
+++ starter-code/main.js
@@ -18,49 +18,63 @@ mongoClient.connect(url, (error, db) => {
     printMenu();
     rl.question('Type an option: ', (option) => {
       switch (option) {
-      case "1":
-      db.collection('companies').find({}, {name: 1, _id: 0}).toArray((error, result) => {
-        if (error) {
-          console.log(error);
-          rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-        } else {
-          console.log(result);
-          rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-        }
-        });
-        break;
-      case "2":
-      db.collection('companies').count((error, result) => {
-        if (error) {
-          console.log(error);
-          rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-        } else {
-          console.log(result);
-          rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-        }
-        });
-        break;
+        case "1":
+          db.collection('companies').find({}, {name: 1, _id: 0}).toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+          });
+          break;
+        case "2":
+          db.collection('companies').count((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+          });
+          break;
         case "3":
-        db.collection('companies').find({founded_year: 2004}, {}).count((error, result) => {
-          if (error) {
-            console.log(error);
-            rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-          } else {
-            console.log(result);
-            rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-          }
+          db.collection('companies').find({founded_year: 2004}, {}).count((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
           });
           break;
         case "4":
-        db.collection('companies').find({founded_year: 2004, founded_month: 2},{name: 1, _id: 0}).toArray((error, result) => {
-          if (error) {
-            console.log(error);
-            rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-          } else {
-            console.log(result);
-            rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
-          }
-          });
+          db.collection('companies').find({founded_year: 2004, founded_month: 2},{name: 1, _id: 0}).toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+            });
+          break;
+        case "5":
+          //in addition to name shows date of foundation to verify that it comes out ordered
+          db.collection('companies').find({$and: [ {"founded_year": 2004},{"founded_month": {$gt: 3}}, {"founded_month": {$lt: 7}}]},
+          {name: 1, _id: 0, founded_day:1,founded_month:1,founded_year:1})
+          .sort({"founded_year": 1, "founded_month": 1, "founded_day": 1}).toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+            });
           break;
         case "0":
           console.log(`👋👋👋👋 😞 \n`);
