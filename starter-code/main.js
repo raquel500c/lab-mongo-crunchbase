c6b3de4a3c1f3cdf6cc3e6fb6399a15eae4274db
raquel500c <raquel500c@gmail.com>
2017-09-16 14:32:18 +0200

    query 6 without city in the projection

--- starter-code/main.js
+++ starter-code/main.js
@@ -76,10 +76,61 @@ mongoClient.connect(url, (error, db) => {
             }
             });
           break;
-        case "6":
-          //in addition to name shows date of foundation to verify that it comes out ordered
+        case "6": //What companies have offices in "Barcelona".
           db.collection('companies').find({"offices.city": "Barcelona"}, {name: 1, _id: 0})
-          .sort({"founded_year": 1, "founded_month": 1, "founded_day": 1}).toArray((error, result) => {
+          .sort({founded_year: 1, founded_month: 1, founded_day: 1}).toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+            });
+          break;
+          //DONE--> PENDIENTE  HACER COMMIT
+        case "7": //List the 10 companies with more employees sorted ascending (show name and employees)
+          db.collection('companies').find({}, {name: 1, description:1, number_of_employees: 1, _id: 0})
+          .limit(10).sort({number_of_employees: -1}).toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+            });
+          break;
+          // DONE --> PENDIENTE  HACER COMMIT
+        case "8": //Find the company with the name "Facebook"
+          db.collection('companies').find({name:"Facebook"}, {name: 1, description:1, _id: 0})
+          .toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+            });
+          break;
+          // DONE --> PENDIENTE  HACER COMMIT
+        case "9": //How many employees has Facebook?
+          db.collection('companies').find({name:"Facebook"}, {name: 1, number_of_employees: 1, _id: 0})
+          .toArray((error, result) => {
+            if (error) {
+              console.log(error);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            } else {
+              console.log(result);
+              rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
+            }
+            });
+          break;
+          //DUDA-- OBJECT OBJECT
+        case "10": //List the name of all the products of Facebook
+          db.collection('companies').find({name:"Facebook"},{'products.name': 1, _id: 0})
+          .toArray((error, result) => {
             if (error) {
               console.log(error);
               rl.question(`\nType enter to continue: `, (answer) => { mainMenu(); });
